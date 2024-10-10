6f21924d5efd1f6ec55d9f14c19cbb5138dc41bf
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-11 00:19:20 +0700

    feat: added reset form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { ProductFormValues } from './components/ProductForm/hook/productFormSchema';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
 import { ProductForm } from './components/ProductForm/ProductForm';
 
 const App = () => {
   const [showAlert, setShowAlert] = useState(false);
+  const [key, setKey] = useState(0);
+
+  const resetForm = useCallback(() => {
+    setKey(prevKey => prevKey + 1);
+  }, []);
 
   const handleSubmit = (data: ProductFormValues) => {
     console.log('Form data:', data);
     setShowAlert(true);
+    resetForm();
 
     // Hide alert after 3 seconds
     setTimeout(() => setShowAlert(false), 3000);
@@ -16,17 +22,17 @@ const App = () => {
 
   return (
     <main className="w-full min-h-screen flex justify-center items-center bg-gray-100">
-      <div className='flex flex-col items-center bg-white p-8 rounded-lg shadow-md max-w-2xl w-full'>
+      <div className='flex flex-col items-center bg-white p-8 shadow-md max-w-xl max-h-screen w-full rounded-3xl'>
         <h1 className="text-3xl font-bold mb-6 text-gray-800">Создание товара</h1>
         {showAlert && (
-          <Alert className="mt-4">
+          <Alert className="m-8 border border-[#8D7FC7]">
             <AlertTitle>Успех</AlertTitle>
             <AlertDescription>
               Форма отправлена успешно
             </AlertDescription>
           </Alert>
         )}
-        <ProductForm onSubmit={handleSubmit} />
+        <ProductForm key={key} onSubmit={handleSubmit}/>
       </div>
     </main>
   );
