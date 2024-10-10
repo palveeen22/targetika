5b137597706e4e732571ffd0b9156fc0fd267be7
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-11 02:20:59 +0700

    adjust: overflow form adjusted

--- src/App.tsx
+++ src/App.tsx
@@ -21,18 +21,20 @@ const App = () => {
   };
 
   return (
-    <main className="w-full min-h-screen flex justify-center items-center bg-gray-100">
-      <div className='flex flex-col items-center bg-white p-8 shadow-md max-w-xl max-h-screen w-full rounded-3xl'>
+    <main className="w-full min-h-screen flex justify-center items-center bg-gray-100 px-8 md:px-0">
+      <div className='flex flex-col items-center bg-white p-8 shadow-md max-w-xl w-full rounded-3xl overflow-hidden'>
         <h1 className="text-3xl font-bold mb-6 text-gray-800">Создание товара</h1>
         {showAlert && (
-          <Alert className="m-8 border border-[#8D7FC7]">
+          <Alert className="mb-6 border border-[#8D7FC7]">
             <AlertTitle>Успех</AlertTitle>
             <AlertDescription>
               Форма отправлена успешно
             </AlertDescription>
           </Alert>
         )}
-        <ProductForm key={key} onSubmit={handleSubmit}/>
+        <div className="w-full max-h-[calc(100vh-200px)] overflow-y-auto">
+          <ProductForm key={key} onSubmit={handleSubmit}/>
+        </div>
       </div>
     </main>
   );
