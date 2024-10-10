96d828fea64f83bf9a9094df4ea5712dbeb711a5
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-11 01:57:50 +0700

    docs: remove console

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -6,9 +6,7 @@ type TProps = {
 
 export const ErrorMessage = ({ error }: TProps) => {
   if (!error) return null;
-
-  console.log(error);
-
+  
   return (
     <p className="text-red-500 text-sm mt-1">
       {('message' in error) ? error.message : error.root?.message}
