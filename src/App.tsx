44a2d4f0be41f5019be7d89fc2a29c0af8fd2127
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2024-10-11 02:32:43 +0700

    adjust: clean up app index

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
-import { useCallback, useState } from 'react';
-import { ProductFormValues } from './components/ProductForm/hook/productFormSchema';
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
-import { ProductForm } from './components/ProductForm/ProductForm';
+import { Fragment } from 'react'
+import ProductPage from './page/ProductPage'
 
 const App = () => {
-  const [showAlert, setShowAlert] = useState(false);
-  const [key, setKey] = useState(0);
-
-  const resetForm = useCallback(() => {
-    setKey(prevKey => prevKey + 1);
-  }, []);
-
-  const handleSubmit = (data: ProductFormValues) => {
-    console.log('Form data:', data);
-    setShowAlert(true);
-    resetForm();
-
-    // Hide alert after 3 seconds
-    setTimeout(() => setShowAlert(false), 3000);
-  };
-
   return (
-    <main className="w-full min-h-screen flex justify-center items-center bg-gray-100 px-8 md:px-0">
-      <div className='flex flex-col items-center bg-white p-8 shadow-md max-w-xl w-full rounded-3xl overflow-hidden'>
-        <h1 className="text-3xl font-bold mb-6 text-gray-800">Создание товара</h1>
-        {showAlert && (
-          <Alert className="mb-6 border border-[#8D7FC7]">
-            <AlertTitle>Успех</AlertTitle>
-            <AlertDescription>
-              Форма отправлена успешно
-            </AlertDescription>
-          </Alert>
-        )}
-        <div className="w-full max-h-[calc(100vh-200px)] overflow-y-auto">
-          <ProductForm key={key} onSubmit={handleSubmit}/>
-        </div>
-      </div>
-    </main>
-  );
-};
+    <Fragment>
+    <ProductPage/>
+    </Fragment>
+  )
+}
 
-export default App;
+export default App
